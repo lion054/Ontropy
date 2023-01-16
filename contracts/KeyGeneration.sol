c905cc30b266872ad6f1de972b24781f98603cfd
The Lion <lionel.siduna@gmail.com>
2023-01-16 23:36:08 +0200

    Update KeyGeneration.sol

--- contracts/KeyGeneration.sol
+++ contracts/KeyGeneration.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/cryptography/ElGamalEncryption.sol";
+
 
 contract KeyGeneration {
     struct Player {
@@ -14,19 +16,29 @@ contract KeyGeneration {
 
     function registerPlayer(uint _threshold) public {
         require(_threshold > 0, "Invalid threshold");
+        require(players[msg.sender].addr == address(0), "Player already registered");
         threshold = _threshold;
         players[msg.sender].addr = msg.sender;
     }
 
     function submitShare(uint _share) public {
+        require(_share <= 100, "Invalid share, share should be less than or equal to 100");
         require(players[msg.sender].addr != address(0), "Player not registered");
         players[msg.sender].share = _share;
     }
+
+    require(sum <= 100, "Invalid sum, sum of shares should be less than or equal to 100");
+
     
     function generateKey() public view returns (bytes32) {
         require(players[msg.sender].share != 0, "Player share not submitted");
         uint count = 0;
         uint sum = 0;
+        if (players[player].addr != address(0) && players[player].share != 0) {
+    count++;
+    sum += players[player].share;
+}
+
         for (address player in players) {
             if (players[player].share != 0) {
                 count++;
